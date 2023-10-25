6bef98c28745fe18137057562da4aa4bcd919023
AMUTUHEIRE MELLON <amutuheiremellon3@gmail.com>
2023-10-25 21:00:59 +0300

    functions and tuples

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -10,11 +10,43 @@
 // console.log(mySize);
 
 
-function calculateTax(income: number, taxYear: number): number {
-    if (taxYear < 2022) 
-        return income * 1.2;
-    return income * 1.3;
-}
+// function calculateTax(income: number, taxYear: number): number {
+//     if (taxYear < 2022) 
+//         return income * 1.2;
+//     return income * 1.3;
+// }
 
 
-calculateTax(10_000, 2022)
+// funtions
+
+// type Employee = {
+//     readonly id: number;
+//     name: string;
+//     retire: (date: Date) => void
+// }
+
+
+
+// let employee: Employee= { 
+//     id: 1, 
+//     name: "Mellon",
+//     retire: (date: Date) => {
+//         console.log(date);
+//     }
+//  };
+
+
+// unoion types
+// we can give a parameter more than one type
+
+
+function kgToLbs(weight: number | string): number {
+    // narrowing
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else 
+        return parseInt(weight) *2.2;
+    }
+    kgToLbs(10);
+    kgToLbs("10kgs");
+ 
